f493e6aee19c78b50be460e14cf83bc2e1f0fb54
ishaanlimaye <ilimaye@purdue.edu>
2025-10-30 18:19:29 -0400

    Reservation class

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,14 @@
-hi
+public class Reservation {
+    private String day;
+    private double time;
+    private User user;
+
+    public Reservation (String day, double time, User user) {
+        this.day = day;
+        this.time = time;
+        this.user = user;
+    }
+
+
+
+}
