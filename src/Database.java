9a463dc8cadf12dccfa1e5cae9608ca4a6697de0
Jaden Fang <jadenfang9@gmail.com>
2025-11-06 18:36:39 -0500

    add reserveration read/write

--- src/Database.java
+++ src/Database.java
@@ -10,12 +10,29 @@ public class Database {
             e.printStackTrace();
         }
     }
-    public void readUsers() {
+    public User[] readUsers() {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(usersFile))) {
-            User[] users = (User[]) ois.readObject();
+            return (User[]) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+        return new User[0];
+    }
+
+    public void saveReservations(Reservation[] reservations) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(reservationsFile))) {
+            oos.writeObject(reservations);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public Reservation[] readReservations() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(reservationsFile))) {
+            return (Reservation[]) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new Reservation[0];
     }
 
     
