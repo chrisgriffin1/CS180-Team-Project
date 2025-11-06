3d22e12814b51bb153c4fb06f39efa9b08913893
Jaden Fang <jadenfang9@gmail.com>
2025-11-06 18:29:07 -0500

    make read and write users

--- src/Database.java
+++ src/Database.java
@@ -1,7 +1,22 @@
-import java.io.*
+import java.io.*;
 public class Database {
     File reservationsFile = new File("reservations.txt");
     File usersFile = new File("users.txt");
+
+    public void saveUsers(User[] users) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(usersFile))) {
+            oos.writeObject(users);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void readUsers() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(usersFile))) {
+            User[] users = (User[]) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     
 
