95785e1c44ba1e858dfd0f6d5014e01c127a75c9
ishaanlimaye <ilimaye@purdue.edu>
2025-11-08 14:39:58 -0500

    Synchronized

--- src/Database.java
+++ src/Database.java
@@ -10,37 +10,41 @@ public class Database {
     File usersFile = new File("users.txt");
 
     public void saveUsers(User[] users) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(usersFile))) {
-            oos.writeObject(users);
-        } catch (IOException e) {
-            e.printStackTrace();
+        synchronized (lock) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(usersFile))) {
+                oos.writeObject(users);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
     public User[] readUsers() {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(usersFile))) {
-            return (User[]) ois.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
+        synchronized (lock) {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(usersFile))) {
+                return (User[]) ois.readObject();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return new User[0];
         }
-        return new User[0];
     }
 
     public void saveReservations(Reservation[] reservations) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(reservationsFile))) {
-            oos.writeObject(reservations);
-        } catch (IOException e) {
-            e.printStackTrace();
+        synchronized (lock) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(reservationsFile))) {
+                oos.writeObject(reservations);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
     public Reservation[] readReservations() {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(reservationsFile))) {
-            return (Reservation[]) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return new Reservation[0];
-    }
-
-    
-
+        synchronized (lock) {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(reservationsFile))) {
+                return (Reservation[]) ois.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return new Reservation[0];
+        } 
 }
