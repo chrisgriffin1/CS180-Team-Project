1d6bfe6ea1a87dda33287919c670140a582a3458
ishaanlimaye <ilimaye@purdue.edu>
2025-11-08 14:53:08 -0500

    2d Array

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -3,17 +3,27 @@ import java.util.ArrayList;
 public class Restaurant {
     private Reservation reservation;
     private int capacity;
+    private Table[][] seatingPlan;
 
 
-    public Restaurant (Reservation reservation, int capacity) {
+    public Restaurant (Reservation reservation, int capacity, Table[][] seatingPlan) {
         this.Reservation = reservation;
         this.capacity = capacity;
+        this.seatingPlan = seatingPlan;
     }
 
     public int getCapacity() { 
         return capacity; 
     }
 
+    public Table[][] getSeatingPlan() {
+        return seatingPlan;
+    }
+
+    public void occupyReservation(Reservation reservation) {
+        for (int i = 0; i < ; i++)
+
+    }
     
 
 }
