b6b19f83ef4a84101c42f11d3407077958080db0
ishaanlimaye <ilimaye@purdue.edu>
2025-11-09 20:38:56 -0500

    changing order of methods reservation interface

--- src/ReservationGuide.java
+++ src/ReservationGuide.java
@@ -1,8 +1,8 @@
 public interface ReservationGuide {
-    boolean isTableOccupied(Table table);
     public String getDay();
     double getTime();
     User getUser();
     Table getTable();
     int getPartySize();
+    boolean isTableOccupied(Table table);
 }
