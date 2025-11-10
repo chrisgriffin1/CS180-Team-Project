166821064bb9a84aeffdd90d28b4078c4401702c
ishaanlimaye <ilimaye@purdue.edu>
2025-11-09 19:30:47 -0500

    Fixing test cases (AssertEquals) in TableTest after making constructor parameter changes

--- src/TableTest.java
+++ src/TableTest.java
@@ -5,34 +5,38 @@ public class TableTest{
 
     @Test(timeout = 1000)
     public void testTableConstructorAndGetters() {
-        Table table = new Table(1, 2);
+        Table table = new Table(1, 2, 2, 8);
 
         assertNotNull("Table object should not be null", table);
-        assertEquals("Table number should be 1", 1, table.getTableNumber());
+        assertEquals("Table row should be 1", 1, table.getTableRow());
+        assertEquals("Table column should be 2", 2, table.getTableColumn());
         assertEquals("Capacity should be 2", 2, table.getCapacity());
     }
 
     @Test(timeout = 1000)
     public void testTableWithZeroCapacity() {
-        Table table = new Table(3, 0);
+        Table table = new Table(3, 1, 4, 10);
 
         assertNotNull("Table object should not be null", table);
-        assertEquals("Capacity should be 0", 0, table.getCapacity());
+        assertEquals("Table row should be 3", 3, table.getTableRow());
+        assertEquals("Table column should be 1", 1, table.getTableColumn());
     }
 
     @Test(timeout = 1000, expected = IllegalArgumentException.class)
     public void testTableWithNegativeCapacity() {
-        Table table = new Table(2, -2);
+        Table table = new Table(2, 5, 2, 5);
 
         assertNotNull(table);
-        assertEquals("Capacity should be -2", -2, table.getCapacity());
+        assertEquals("Table row should be 2", 2, table.getTableRow());
+        assertEquals("Table column should be 5", 5, table.getTableColumn());
     }
 
     @Test(timeout = 1000, expected = IllegalArgumentException.class)
     public void testTableWithExcessiveCapacity() {
-        Table table = new Table(4, 5);
+        Table table = new Table(4, 5, 6, 8);
 
         assertNotNull(table);
-        assertEquals("Capacity should be 5", 5, table.getCapacity());
+        assertEquals("Table row should be 4", 4, table.getTableRow());
+        assertEquals("Table column should be 5", 5, table.getTableColumn());
     }
 }
