846df0043f1d77d888554a1eb414fd78d21a8e1e
Jaden Fang <jadenfang9@gmail.com>
2025-11-20 22:42:53 -0500

    create basic server logic for new user

--- src/Server.java
+++ src/Server.java
@@ -27,14 +27,23 @@ public class Server implements Runnable {
                 try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
                     String command = reader.readLine();
+                    
+                    // new user command are in the format "new user;username password"
 
-                    if (command.equals("new user")) {
-                        db.makeNewUser();
-                    } else if (command.equals("delete user")) {
+                    // new reservation command are in the format 
+                    // "new reservation;day time username partySize tableRow tableColumn"
+
+
+
+                    if (command.contains("new user")) {
+                        String[] data = command.split(";");
+                        String[] credentials = data[1].split(" ");
+                        db.makeNewUser(credentials[0], credentials[1]);
+                    } else if (command.contains("delete user")) {
                         db.deleteUser();
-                    } else if (command.equals("new reservation")) {
+                    } else if (command.contains("new reservation")) {
                         db.createReservation();
-                    } else if (command.equals("delete reservation")) {
+                    } else if (command.contains("delete reservation")) {
                         db.deleteReservation();
                     } else {
                         writer.println("Invalid command");
