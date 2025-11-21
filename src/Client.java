a16ad649c49cb77098a500762db9b8ccdd0d63ba
ishaanlimaye <ilimaye@purdue.edu>
2025-11-20 22:43:09 -0500

    Client run method with JPanel, log in and create new user buttons

--- src/Client.java
+++ src/Client.java
@@ -1,37 +1,95 @@
 import javax.swing.*;
 import java.io.*;
 import java.net.*;
-import java.io.*;
 import java.lang.reflect.Array;
-import java.net.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 import javax.swing.JOptionPane;
+
 import static java.lang.Integer.parseInt;
 
-public class Client {
+public class Client extends JComponent implements Runnable {
+    private Image image;
+    private int curX;
+    private int curY;
+    private int oldX;
+    private int oldY;
+    Graphics2D graphics2D;
+    private JButton logInButton;
+    private JButton createNewUserButton;
+    private Color penColor = Color.black;
+    Paint paint;
+
     
     public static void main(String[] args) throws UnknownHostException, IOException {
 
-
-        JOptionPane.showMessageDialog(null, "Welcome to the restaurant!",
+        try {   
+            JOptionPane.showMessageDialog(null, "Welcome to the restaurant!",
                     "Search Engine", JOptionPane.INFORMATION_MESSAGE);
 
-        
-        try{            
-            
+            Scanner scanner = new Scanner(System.in);
             Socket socket = new Socket("localhost", 47906);
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
 
             JOptionPane.showMessageDialog(null, "Connection established!",
                     "Search Engine", JOptionPane.INFORMATION_MESSAGE);
-        }catch (UnknownHostException uhe) {
+        
+            
+                
+                
 
+        public void run() {
+            JFrame frame = new JFrame("Paint"); //change if needed.
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(900, 700);
+            frame.setLocationRelativeTo(null);
+            Container content = frame.getContentPane();
+            content.setLayout(new BorderLayout());
+            logInButton = new JButton("Log In");
+            createNewUserButton = new JButton("Create New User");
+            clearButton.addActionListener(actionListener);
+            fillButton.addActionListener(actionListener);
+            
+
+        }
+
+        /* public static void main(String[] args) {
+            SwingUtilities.invokeLater();
+        } */    
+        
+
+        ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == logInButton) {
+                
+            } 
+            
+            if (e.getSource() == createNewUserButton) {
+
+            } 
+
+
+
+
+
+
+        }
         }        
+
+        } catch (UnknownHostException uhe) {
+            uhe.printStackTrace();
+        }
+        
+        
+
+
     }
 
 
