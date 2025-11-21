cdddfdc63f9a30bba3eb0b3bc103972cb9fb8ebd
Jaden Fang <jadenfang9@gmail.com>
2025-11-20 22:10:35 -0500

    beginnning of server

--- src/Server.java
+++ src/Server.java
@@ -1,16 +1,45 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.net.Socket;
+import javax.swing.JOptionPane;
 
-public Server implements Runnable {
+public class Server implements Runnable {
     
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(47906);
+    public void run() {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(47906);
+
+        } catch (IOException e) {
+            System.out.println("Could not listen on port: 47906");
+            
+        }
+
         System.out.println("Server is listening on port 47906");
-        Socket socket = serverSocket.accept();
-        System.out.println("Client connected");
     
-        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+        
+        while (true) {
+            Socket socket = null;
+            try {
+                socket = serverSocket.accept();
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+                    String clientMessage = reader.readLine();
+
+                } catch (IOException e) {
+                    System.out.println("Client  error");
+                } finally {
+                    if (socket != null) {
+                        socket.close();
+                    }
+                }
+
+            } catch (Exception e) {
+                
+            }
+        }
+    }
 
         
 
@@ -21,5 +50,3 @@ public Server implements Runnable {
 
 
     }
-
-}
