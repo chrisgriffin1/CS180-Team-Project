99b0d74552ae77b2f2099e749ecf1173b3b58949
Sktch25 <aop2557@gmail.com>
2025-11-20 22:58:40 -0500

    huh

--- src/Server.java
+++ src/Server.java
@@ -27,23 +27,14 @@ public class Server implements Runnable {
                 try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
                     String command = reader.readLine();
-                    
-                    // new user command are in the format "new user;username password"
 
-                    // new reservation command are in the format 
-                    // "new reservation;day time username partySize tableRow tableColumn"
-
-
-
-                    if (command.contains("new user")) {
-                        String[] data = command.split(";");
-                        String[] credentials = data[1].split(" ");
-                        db.makeNewUser(credentials[0], credentials[1]);
-                    } else if (command.contains("delete user")) {
+                    if (command.equals("New user")) {
+                        db.makeNewUser();
+                    } else if (command.equals("Delete user")) {
                         db.deleteUser();
-                    } else if (command.contains("new reservation")) {
+                    } else if (command.equals("New reservation")) {
                         db.createReservation();
-                    } else if (command.contains("delete reservation")) {
+                    } else if (command.equals("Delete reservation")) {
                         db.deleteReservation();
                     } else {
                         writer.println("Invalid command");
